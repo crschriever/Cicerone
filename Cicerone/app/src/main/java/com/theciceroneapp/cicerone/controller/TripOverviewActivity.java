62f7701acd4611cd7c328602c4a842ff77d94245
Rikesh Subedi <rsubedi3@gatech.edu>
2017-10-14 02:24:36 -0400

    Create activity for trip overview

---
+++ Cicerone/app/src/main/java/com/theciceroneapp/cicerone/controller/TripOverviewActivity.java
@@ -0,0 +1,15 @@
+package com.theciceroneapp.cicerone.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.theciceroneapp.cicerone.R;
+
+public class TripOverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_overview);
+    }
+}
