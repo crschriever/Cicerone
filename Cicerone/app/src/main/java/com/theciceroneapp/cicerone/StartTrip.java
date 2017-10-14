79cd9f5dd27284666a2db9d2f153ae289cc7232b
Rikesh Subedi <rsubedi3@gatech.edu>
2017-10-13 22:17:16 -0400

    Create App Repository

---
+++ Cicerone/app/src/main/java/com/theciceroneapp/cicerone/StartTrip.java
@@ -0,0 +1,13 @@
+package com.theciceroneapp.cicerone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartTrip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_trip);
+    }
+}
