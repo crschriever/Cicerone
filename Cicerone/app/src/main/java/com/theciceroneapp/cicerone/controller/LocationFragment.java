1cbc2ae097b6cce213c3e355ad12336cc290c290
Rikesh Subedi <rsubedi3@gatech.edu>
2017-10-14 20:31:59 -0400

    Attempt new tabbing method with fragments

--- Cicerone/app/src/main/java/com/theciceroneapp/cicerone/controller/LocationFragment.java
+++ Cicerone/app/src/main/java/com/theciceroneapp/cicerone/controller/LocationFragment.java
@@ -19,14 +19,9 @@ import com.theciceroneapp.cicerone.R;
  * create an instance of this fragment.
  */
 public class LocationFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_LOCATION = "ARG_LOCATION";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private int mLocation;
 
     private OnFragmentInteractionListener mListener;
 
@@ -38,16 +33,16 @@ public class LocationFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param location index needed from trip
      * @return A new instance of fragment LocationFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static LocationFragment newInstance(String param1, String param2) {
-        LocationFragment fragment = new LocationFragment();
+    public static LocationFragment newInstance(int location) {
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
+        args.putInt(ARG_LOCATION, location);
+
+        LocationFragment fragment = new LocationFragment();
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -56,8 +51,7 @@ public class LocationFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mLocation = getArguments().getInt(ARG_LOCATION);
         }
     }
 
@@ -65,7 +59,14 @@ public class LocationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location, container, false);
+        View view = inflater.inflate(R.layout.fragment_location, container, false);
+
+        // SET VALUES FOR UI ELEMENTS HERE
+        // Ex: TextView textView = (TextView) view;
+        //     textView.setText("Fragment #" + mLocation);
+        //     return view;
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
---
+++ Cicerone/app/src/main/java/com/theciceroneapp/cicerone/controller/LocationFragmentPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.theciceroneapp.cicerone.controller;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by keshr on 10/14/2017.
+ */
+
+public class LocationFragmentPagerAdapter extends FragmentPagerAdapter {
+    final int PAGE_COUNT = 2;
+    private String tabTitles[] = new String[] { "Overview", "Current Location"};
+    private Context context;
+
+    public LocationFragmentPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return LocationFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+}
